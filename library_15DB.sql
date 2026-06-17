86918ac05588c4aef37e2137f24ee043b5794e09
jang.jiwoo <jang.jiwoo@ncd.co.jp>
2026-06-17 14:02:11 +0900

    change date

--- library_15DB.sql
+++ library_15DB.sql
@@ -29,7 +29,7 @@ CREATE TABLE cataloglist (
     publisher TEXT,
     publish_date DATE
 );
-INSERT INTO cataloglist VALUES('1234567890','開発演習',1,'ジャン','チームやろう',DATE'2026-06-17');
+INSERT INTO cataloglist VALUES('4163918272','センスの哲学',1,'千葉 雅也','文藝春秋',DATE '20240405');
 
 
 DROP TABLE IF EXISTS categorylist;
@@ -63,9 +63,9 @@ CREATE TABLE memberlist(
 );
 
 
-INSERT INTO memberlist VALUES(1,'山田太郎','〒111-1111東京','000-000-000','a@example.com',DATE '1999-01-02',DATE '2010-1-01',DATE '2013-11-21');
-INSERT INTO memberlist VALUES(2,'田中一郎','〒111-1112東京','000-000-111','b@example.com',DATE '2000-01-04',DATE '2012-1-01',DATE '2013-12-02');
-INSERT INTO memberlist VALUES(3,'山本次郎','〒111-1113東京','000-000-222','c@example.com',DATE '2001-02-02',DATE '2014-1-01',DATE '2019-09-11');
+INSERT INTO memberlist VALUES(1,'山田太郎','〒111-1111東京','000-000-000','a@example.com',DATE '19990102',DATE '2010101',DATE '20131121');
+INSERT INTO memberlist VALUES(2,'田中一郎','〒111-1112東京','000-000-111','b@example.com',DATE '20000104',DATE '2012101',DATE '20131202');
+INSERT INTO memberlist VALUES(3,'山本次郎','〒111-1113東京','000-000-222','c@example.com',DATE '20010202',DATE '2014101',DATE '20190911');
 
 ALTER TABLE categorylist OWNER TO postgres;
 ALTER TABLE memberlist OWNER TO postgres;
@@ -84,9 +84,9 @@ CREATE TABLE rentlist
   remarks TEXT
 );
 
-INSERT INTO rentlist VALUES(1,1, 100, DATE '2004-03-22', DATE '2026-06-17',null,null);
-INSERT INTO rentlist VALUES(2,2, 110, DATE '2004-04-22', DATE '2026-06-27',null,null);
-INSERT INTO rentlist VALUES(3,3, 120, DATE '2004-05-22', DATE '2026-06-14',null,null);
+INSERT INTO rentlist VALUES(1,1, 100, DATE '20040322', DATE '20260617',null,null);
+INSERT INTO rentlist VALUES(2,2, 110, DATE '20040422', DATE '20260627',null,null);
+INSERT INTO rentlist VALUES(3,3, 120, DATE '20040522', DATE '20260614',null,null);
 
 
 DROP TABLE IF EXISTS stocklist;
@@ -104,9 +104,9 @@ CREATE TABLE stocklist
 
 
 
-INSERT INTO stocklist VALUES(1, '732189',DATE '2026-06-17',null,null);
-INSERT INTO stocklist VALUES(2, '479811',DATE '2026-06-17',null,null);
-INSERT INTO stocklist VALUES(3, '579831',DATE '2026-06-17',null,null);
+INSERT INTO stocklist VALUES(1, '732189',DATE '20260617',null,null);
+INSERT INTO stocklist VALUES(2, '479811',DATE '20260617',null,null);
+INSERT INTO stocklist VALUES(3, '579831',DATE '20260617',null,null);
 
 ALTER TABLE rentlist OWNER TO postgres;
 ALTER TABLE stocklist OWNER TO postgres;
