11e673b6ad564c32ea96682ac67b2b1e01adf3f7
jang.jiwoo <jang.jiwoo@ncd.co.jp>
2026-06-17 10:51:32 +0900

    add DB

--- Re.library_15DB.sql
+++ Re.library_15DB.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS library_15;
-DROP USER IF EXISTS postgre;
-CREATE USER postgre WITH PASSWORD 'himitu';
-CREATE DATABASE library_15 OWNER postgre ENCODING 'UTF8';
+DROP USER IF EXISTS postgres;
+CREATE USER postgres WITH PASSWORD 'himitu';
+CREATE DATABASE library_15 OWNER postgres ENCODING 'UTF8';
 \c library_15
 
 DROP TABLE IF EXISTS adminlist;
@@ -31,7 +31,7 @@ CREATE TABLE cataloglist (
     publisher TEXT,
     publish_date DATE
 );
--- INSERT INTO cataloglist VALUES(1234567890,'開発演習',1,'ジャン','チームやろう',DATE'2026-06-17');
+
 
 
 DROP TABLE IF EXISTS categorylist;
@@ -39,7 +39,6 @@ CREATE TABLE categorylist(
   category_code SERIAL PRIMARY KEY,
   category_name TEXT
 );
-
 DROP TABLE IF EXISTS memberlist;
 CREATE TABLE memberlist(
   member_id SERIAL PRIMARY KEY,
@@ -52,51 +51,42 @@ CREATE TABLE memberlist(
   member_canceldate DATE
 );
 
-INSERT INTO categorylist VALUES(0,'総記');
-INSERT INTO categorylist VALUES(1,'哲学');
-INSERT INTO categorylist VALUES(2,'哲学');
 
 
--- INSERT INTO memberlist VALUES(1,'山田太郎','〒111-1111東京','000-000-000','a@example.com',DATE '1999-01-02',DATE '2010-1-01',DATE '2013-11-21');
--- INSERT INTO memberlist VALUES(2,'田中一郎','〒111-1112東京','000-000-111','b@example.com',DATE '2000-01-04',DATE '2012-1-01',DATE '2013-12-02');
-INSERT INTO memberlist VALUES(3,'山本次郎','〒111-1113東京','000-000-222','c@example.com',DATE '2001-02-02',DATE '2014-1-01',DATE '2019-09-11');
 
-ALTER TABLE categorylist OWNER TO postgre;
-ALTER TABLE memberlist OWNER TO postgre;
-ALTER TABLE adminlist OWNER TO postgre;
-ALTER TABLE cataloglist OWNER TO postgre;
+ALTER TABLE categorylist OWNER TO postgres;
+ALTER TABLE memberlist OWNER TO postgres;
+ALTER TABLE adminlist OWNER TO postgres;
+ALTER TABLE cataloglist OWNER TO postgres;
 
 DROP TABLE IF EXISTS rentlist;
+DROP TABLE IF EXISTS stocklist;
+
 CREATE TABLE rentlist
 (
-  member_id SERIAL PRIMARY KEY,
+  member_id INTEGER SERIAL PRIMARY KEY,
   book_id INTEGER,
   rent_date DATE,
   return_deadline DATE,
   return_date DATE,
   note TEXT
 );
+ALTER TABLE rentlist OWNER TO postgres;
 
-DROP TABLE IF EXISTS stocklist;
+
 CREATE TABLE stocklist
 (
   book_id SERIAL PRIMARY KEY,
-  isbn TEXT,
-  arrival_date DATE,
-  discard_date DATE,
-  note TEXT
+  isbn TEXT
+  arrival_date DATE
+  discard_date DATE
+  note RETURN
 );
-
-
-
-
+ALTER TABLE stocklist OWNER TO postgres;
 INSERT INTO rentlist VALUES(1, 100, DATE '2004-03-22', DATE '2026-06-17',null,null);
 INSERT INTO rentlist VALUES(2, 110, DATE '2004-04-22', DATE '2026-06-27',null,null);
 INSERT INTO rentlist VALUES(3, 120, DATE '2004-05-22', DATE '2026-06-14',null,null);
 
-INSERT INTO rentlist VALUES(1, 732189,DATE '2026-06-17',null,null);
-INSERT INTO rentlist VALUES(1, 479811,DATE '2026-06-17',null,null);
-INSERT INTO rentlist VALUES(1, 579831,DATE '2026-06-17',null,null);
-
-ALTER TABLE rentlist OWNER TO postgres;
-ALTER TABLE stocklist OWNER TO postgres;
+INSERT INTO rentlist(book_id, isbn, arrival_date, discard_date, note) VALUES(1, 732189,DATE '2026-06-17',null,null);
+INSERT INTO rentlist(book_id, isbn, arrival_date, discard_date, note) VALUES(1, 479811,DATE '2026-06-17',null,null);
+INSERT INTO rentlist(book_id, isbn, arrival_date, discard_date, note) VALUES(1, 579831,DATE '2026-06-17',null,null);
