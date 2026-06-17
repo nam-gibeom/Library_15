602ef6429bfb4d3a1e32b617c80a0db0737ccb15
jang.jiwoo <jang.jiwoo@ncd.co.jp>
2026-06-17 11:19:01 +0900

    add category

--- library_15DB.sql
+++ library_15DB.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS library_15;
-DROP USER IF EXISTS postgre;
-CREATE USER postgre WITH PASSWORD 'himitu';
-CREATE DATABASE library_15 OWNER postgre ENCODING 'UTF8';
+DROP USER IF EXISTS postgres;
+CREATE USER postgres WITH PASSWORD 'himitu';
+CREATE DATABASE library_15 OWNER postgres ENCODING 'UTF8';
 \c library_15
 
 DROP TABLE IF EXISTS adminlist;
@@ -52,17 +52,25 @@ CREATE TABLE memberlist(
 
 INSERT INTO categorylist VALUES(0,'総記');
 INSERT INTO categorylist VALUES(1,'哲学');
-INSERT INTO categorylist VALUES(2,'哲学');
+INSERT INTO categorylist VALUES(2,'歴史');
+INSERT INTO categorylist VALUES(3,'社会科学');
+INSERT INTO categorylist VALUES(4,'自然科学');
+INSERT INTO categorylist VALUES(5,'技術');
+INSERT INTO categorylist VALUES(6,'産業');
+INSERT INTO categorylist VALUES(7,'芸術');
+INSERT INTO categorylist VALUES(8,'言語');
+INSERT INTO categorylist VALUES(9,'文学');
+
 
 
 INSERT INTO memberlist VALUES(1,'山田太郎','〒111-1111東京','000-000-000','a@example.com',DATE '1999-01-02',DATE '2010-1-01',DATE '2013-11-21');
 INSERT INTO memberlist VALUES(2,'田中一郎','〒111-1112東京','000-000-111','b@example.com',DATE '2000-01-04',DATE '2012-1-01',DATE '2013-12-02');
 INSERT INTO memberlist VALUES(3,'山本次郎','〒111-1113東京','000-000-222','c@example.com',DATE '2001-02-02',DATE '2014-1-01',DATE '2019-09-11');
 
-ALTER TABLE categorylist OWNER TO postgre;
-ALTER TABLE memberlist OWNER TO postgre;
-ALTER TABLE adminlist OWNER TO postgre;
-ALTER TABLE cataloglist OWNER TO postgre;
+ALTER TABLE categorylist OWNER TO postgres;
+ALTER TABLE memberlist OWNER TO postgres;
+ALTER TABLE adminlist OWNER TO postgres;
+ALTER TABLE cataloglist OWNER TO postgres;
 
 DROP TABLE IF EXISTS rentlist;
 CREATE TABLE rentlist
