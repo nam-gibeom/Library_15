844b91f1399a5a296a7f8198114daa224fad9c6c
jang.jiwoo <jang.jiwoo@ncd.co.jp>
2026-06-17 16:55:38 +0900

    add member bean and dao

---
+++ Library_15_code/Library_15/src/main/java/java/la/bean/MemberBean.java
@@ -0,0 +1,116 @@
+package la.bean;
+
+public class MemberBean {
+private int member_id;
+private String member_name;
+private String member_address;
+private String member_tel;
+private String member_mail;
+private String member_birth;
+private String member_registdate;
+private String current_date;
+
+
+public MemberBean(int member_id, String member_name, String member_address, String member_tel, String member_mail,
+		String member_birth, String member_registdate, String current_date) {
+	
+	this.member_id = member_id;
+	this.member_name = member_name;
+	this.member_address = member_address;
+	this.member_tel = member_tel;
+	this.member_mail = member_mail;
+	this.member_birth = member_birth;
+	this.member_registdate = member_registdate;
+	this.current_date = current_date;
+}
+public MemberBean(int member_id, String member_name) {
+	this.member_id = member_id;
+	this.member_name = member_name;
+}
+
+public int getMember_id() {
+	return member_id;
+}
+
+
+public void setMember_id(int member_id) {
+	this.member_id = member_id;
+}
+
+
+public String getMember_name() {
+	return member_name;
+}
+
+
+public void setMember_name(String member_name) {
+	this.member_name = member_name;
+}
+
+
+public String getMember_address() {
+	return member_address;
+}
+
+
+public void setMember_address(String member_address) {
+	this.member_address = member_address;
+}
+
+
+public String getMember_tel() {
+	return member_tel;
+}
+
+
+public void setMember_tel(String member_tel) {
+	this.member_tel = member_tel;
+}
+
+
+public String getMember_mail() {
+	return member_mail;
+}
+
+
+public void setMember_mail(String member_mail) {
+	this.member_mail = member_mail;
+}
+
+
+public String getMember_birth() {
+	return member_birth;
+}
+
+
+public void setMember_birth(String member_birth) {
+	this.member_birth = member_birth;
+}
+
+
+public String getMember_registdate() {
+	return member_registdate;
+}
+
+
+public void setMember_registdate(String member_registdate) {
+	this.member_registdate = member_registdate;
+}
+
+
+public String getCurrent_date() {
+	return current_date;
+}
+
+
+public void setCurrent_date(String current_date) {
+	this.current_date = current_date;
+}
+
+
+
+	
+}
+    
+    
+
---
+++ Library_15_code/Library_15/src/main/java/java/la/dao/MemberDAO.java
@@ -0,0 +1,133 @@
+package la.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import la.bean.MemberBean;
+
+public class MemberDAO {
+	private String url = "jdbc:postgresql:library_15";
+    private String user = "postgres";
+    private String pass = "himitu";
+
+    public MemberDAO() throws DAOException {
+        try {
+			// JDBCドライバの登録
+			Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new DAOException("JDBCドライバの登録に失敗しました。");
+        }
+    }
+    public List<MemberBean> getIdName() throws DAOException {
+        // SQL文の作成
+        String sql = "select member_id, member_name from memberlist order by member_id desc limit 1";
+		
+        try (// データベースへの接続
+             Connection con = DriverManager.getConnection(url, user, pass);
+			 // PreparedStatementオブジェクトの取得
+			 PreparedStatement st = con.prepareStatement(sql);
+			 // SQLの実行
+			 ResultSet rs = st.executeQuery();) {
+            // 結果の取得および表示
+			List<MemberBean> list = new ArrayList<MemberBean>();
+			while (rs.next()) {
+			    int id = rs.getInt("id");
+			    String name = rs.getString("name");
+			    MemberBean bean = new MemberBean(id, name);
+			    list.add(bean);
+			    }
+			return list;
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+			}
+        }
+
+    public List<MemberBean> findAll() throws DAOException {
+        // SQL文の作成
+        String sql = "Select * from memberlist where member_id = ?";
+		
+        try (// データベースへの接続
+             Connection con = DriverManager.getConnection(url, user, pass);
+			 // PreparedStatementオブジェクトの取得
+			 PreparedStatement st = con.prepareStatement(sql);
+			 // SQLの実行
+			 ResultSet rs = st.executeQuery();) {
+            // 結果の取得および表示
+			List<MemberBean> list = new ArrayList<MemberBean>();
+			while (rs.next()) {
+			    int id = rs.getInt("member_id");
+			    String name = rs.getString("member_name");
+			    String address = rs.getString("member_address");
+			    String tel = rs.getString("member_tel");
+			    String mail = rs.getString("member_mail");
+			    String birth = rs.getString("member_birth");
+			    String registdate = rs.getString("member_registdate");
+			    String canceldate = rs.getString("member_canceldate");
+			    
+			    MemberBean bean = new MemberBean(id, name,address,tel,mail,birth,registdate,canceldate);
+			    list.add(bean);
+			    
+			}
+			return list;
+			}catch (SQLException e) {
+				e.printStackTrace();
+				throw new DAOException("レコードの取得に失敗しました。");
+			}
+    }
+
+    public void addMember(String member_name, String member_address, String member_tel, String member_mail, String member_birth, String current_date) 
+    		throws DAOException {
+		// SQL文の作成
+		String sql = "insert into memberlist(member_name, member_address, member_tel, member_mail, member_birth, member_registdate) values(?, ?, ?, ?, ?, ?)";
+		
+		try (// データベースへの接続
+			 Connection con = DriverManager.getConnection(url, user, pass);
+			 // PreparedStatementオブジェクトの取得
+			 PreparedStatement st = con.prepareStatement(sql);) {
+			// 商品名と値段の指定
+			st.setString(1, member_name);
+			st.setString(2, member_address);
+			st.setString(3, member_tel);
+			st.setString(4, member_mail);
+			st.setString(5, member_birth);
+			st.setString(6, current_date);
+			// SQLの実行
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		} 
+	
+	}
+    
+    public void updateCancelDate(int member_id, String current_date) throws DAOException {
+		// SQL文の作成
+		String sql = "update memberlist set member_canceldate = ? where member_id = ?";
+		
+		try (// データベースへの接続
+			 Connection con = DriverManager.getConnection(url, user, pass);
+			 // PreparedStatementオブジェクトの取得
+			 PreparedStatement st = con.prepareStatement(sql);) {
+			// 商品名と値段の指定
+			st.setString(1, current_date);
+			st.setInt(2, member_id);
+			// SQLの実行
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの操作に失敗しました。");
+		} 
+	
+	}
+    
+}
+
+    
+
