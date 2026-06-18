57ceba172382bd4237df9b0cbb19600d0355552d
shoei.taira <shoei.taira@ncdsol.co.jp>
2026-06-18 10:38:54 +0900

    menu

---
+++ Library_15_code/Library_15/src/main/java/la/Servlet/MenuServlet.java
@@ -0,0 +1,61 @@
+package la.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.service.MenuService;
+import la.service.ServiceException;
+
+
+/**
+ * Servlet implementation class MenuServlet
+ */
+@WebServlet("/MenuServlet")
+public class MenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MenuServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			
+				String id = "26011";
+				String pass = "26011";
+            
+                MenuService dao = new MenuService();
+                String name = dao.Login(id,pass);
+                
+                
+                System.out.println(name);
+                
+                
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            request.setAttribute("message", "内部エラーが発生しました。");
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
