738e2f281c91356b4ec036d0ae1d4c58c7fd69f5
nam.gibeom <nam.gibeom@ncd.co.jp>
2026-06-19 16:53:57 +0900

    add BookServlet

--- Library_15_code/Library_15/src/main/java/la/test/BookServiceTest.java
+++ Library_15_code/Library_15/src/main/java/la/test/BookServiceTest.java
@@ -1,68 +1,29 @@
 package la.test;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
-import la.Bean.catalogListBean;
-import la.Dao.BookDAO;
 import la.Dao.DAOException;
-import la.Service.BookService;
 
 public class BookServiceTest {
 
 	public static void main(String[] args) throws DAOException {
-		BookDAO dao = new BookDAO();
-		BookService service = new BookService();
 		
-		String type = "title";
-		String value = "存在";
+		String publish_date = "2026-03-20";
+		String current_date = "2026-06-19";
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate Publish = LocalDate.parse(publish_date, format);
+		LocalDate Current = LocalDate.parse(current_date, format);
+		Period.between(Publish, Current);
 		
-		String type1 = null;
-		String value1 = null;
-		
-		if (type.equals("book_id")) {
-			type1 = "isbn";
-			value1 = dao.getIsbnByBook((Integer.parseInt(value)));
-		} else if (type.equals("isbn")) {
-			type1 = "isbn";
-			value1 = value;
-		} else if (type.equals("title")) {
-			type1 = "title";
-			value1 = value;
-		}
-		
-		List<catalogListBean> list = dao.getCatalogListInfo(type1, value1);
-		for (catalogListBean bean : list ) {
-			int canRentDay;
-			String current_date = service.getCurrentDate();
-			int period = service.CalculatePeriod(bean.getPublish_date(), current_date);
-			if (period <= 3) {
-				canRentDay = 10;
-			} else {
-				canRentDay = 15;
-			}
-		
-			
-			String title = bean.getTitle();
-			
-			String category_name = dao.getCategoryName(bean.getCategory_code());
-			String publish_date = bean.getPublish_date();
-			
-			int count_book = dao.getBookIdByIsbn(bean.getIsbn()).size();
-
-			int count_rented_book = dao.getRentedBookCountByIsbn(bean.getIsbn());
-			System.out.println(count_rented_book);
-			
-			String rentAvailable = (count_book - count_rented_book) + "/" + count_book;
-			System.out.println(rentAvailable);
-			
-			// canRentDay
-			
-			
-		}	
-			
-			
-			 
+		if ((Period.between(Publish, Current).getYears() >= 1) || (Period.between(Publish, Current).getYears() == 0) && Period.between(Publish, Current).getMonths() >= 3) {
+			System.out.println("三ヶ月以上！");
+		} else {
+			System.out.println("失敗");
 		}
 		
 	}
-
+	
+	
+}
